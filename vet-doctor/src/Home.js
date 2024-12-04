c801c1e255f4240932ae8da6c6c0490c2f3b26f7
bartosz <bartosz@bartoszs-MacBook-Pro.local>
2024-12-04 09:27:19 +0100

    dev4passion-vet-doctor upload-test

---
+++ vet-doctor/src/Home.js
@@ -0,0 +1,21 @@
+import Reart, { useState } from 'react';
+
+const Home = () => {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const handleLogin = async (e) => {
+
+
+    }
+
+    return (
+
+        <div className="small-container">
+          <h1>home</h1>
+        </div>
+    )
+}
+
+export default Home;
